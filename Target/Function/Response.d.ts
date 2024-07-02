c9780973838239edf6f8a229005222c1bc937f3d
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-02 12:35:08 +0300

    squash!

---
+++ Target/Function/Response.d.ts
@@ -0,0 +1,8 @@
+/**
+ * @module Response
+ *
+ */
+declare const _default: Interface;
+export default _default;
+import type Interface from "../Interface/Response.js";
+export declare const Response: typeof import("@cloudflare/workers-types/experimental/index.js").Response;
---
+++ Target/Variable/Worker.d.ts
@@ -0,0 +1,13 @@
+/**
+ * @module Worker
+ *
+ */
+declare const _default: Interface;
+export default _default;
+export declare const Access: import("@codeeditorland/common/Target/Interface/Access.js").default;
+export declare const Put: import("@codeeditorland/common/Target/Interface/Put.js").default;
+export declare const WebSocketPair: new () => {
+    0: import("@cloudflare/workers-types/experimental/index.js").WebSocket;
+    1: import("@cloudflare/workers-types/experimental/index.js").WebSocket;
+};
+import type Interface from "../Interface/Worker.js";
