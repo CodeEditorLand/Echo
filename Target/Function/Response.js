3654140a2f88ac67c431f1f2098231d754620657
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-05 09:43:32 +0300

    squash!

--- Target/Function/Response.js
+++ Target/Function/Response.js
@@ -1,9 +1,1 @@
-var s = async (...[e = null, t = 200]) =>
-	new a(JSON.stringify(e), {
-		status: t,
-		headers: { "Content-Type": "application/json;charset=utf-8" },
-	});
-const { Response: a } = await import(
-	"@cloudflare/workers-types/experimental/index.js"
-);
-export { a as Response, s as default };
+var s=async(...[e=null,t=200])=>new a(JSON.stringify(e),{status:t,headers:{"Content-Type":"application/json;charset=utf-8"}});const{Response:a}=await import("@cloudflare/workers-types/experimental/index.js");export{a as Response,s as default};
