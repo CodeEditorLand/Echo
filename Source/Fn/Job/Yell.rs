9ce270b3611f2ddca89b08daee590db6629db3e0
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-07 11:43:35 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Source/Fn/Job/Yell.rs
+++ Source/Fn/Job/Yell.rs
@@ -4,6 +4,22 @@ use futures::{SinkExt, StreamExt};
 use std::sync::Arc;
 use tokio_tungstenite::tungstenite::Message;
 
+/// Asynchronously processes WebSocket messages and actions from a work queue.
+///
+/// # Arguments
+///
+/// * `Order` - A WebSocket stream used for reading and writing messages.
+/// * `Work` - An `Arc` reference to a `Work` instance that contains the queue of actions to be processed.
+/// * `Receipt` - An `Arc` reference to a mutex-protected unbounded receiver channel for receiving action results.
+///
+/// # Behavior
+///
+/// This function runs an infinite loop where it uses `tokio::select!` to concurrently:
+/// 1. Read messages from the WebSocket stream. If a message is received and successfully parsed into an `Action`,
+///    it is assigned to the work queue.
+/// 2. Receive action results from the `Receipt` channel and send them back through the WebSocket stream.
+///
+/// If sending a message through the WebSocket stream fails, the loop breaks.
 pub async fn Fn(
 	Order: tokio_tungstenite::WebSocketStream<tokio::net::TcpStream>,
 	Work: Arc<Work>,
